98b7fb01ef7e5dc9c925f15d3b7ec52469f56639
burst9 <burst@net9.org>
2015-10-30 20:26:59 +0800

    add test resource

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -7,6 +7,8 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
+    
+    FileUtils::getInstance()->addSearchPath("res");
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
