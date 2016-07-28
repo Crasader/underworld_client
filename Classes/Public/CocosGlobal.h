c4b7481aaf3b989ad1894f0e57bd797f2bf34305
wenchengye <wenchengye1005@163.com>
2016-07-28 17:37:02 +0800

    update core

--- Classes/Public/CocosGlobal.h
+++ Classes/Public/CocosGlobal.h
@@ -29,14 +29,14 @@
 #define BIG_FONT_SIZE       (24.0f)
 #define TITLE_FONT_SIZE     (40.0f)
 
-#define LAYER_DEFAULT_COLOR (Color4B(0, 0, 0, 0))
-#define LAYER_MASK_COLOR    (Color4B(0, 0, 0, 128))
+#define LAYER_DEFAULT_COLOR (cocos2d::Color4B(0, 0, 0, 0))
+#define LAYER_MASK_COLOR    (cocos2d::Color4B(0, 0, 0, 128))
 
-#define GOLD_LABEL_COLOR    (Color4B(255, 246, 0, 255))
-#define WOOD_LABEL_COLOR    (Color4B(0, 228, 255, 255))
+#define GOLD_LABEL_COLOR    (cocos2d::Color4B(255, 246, 0, 255))
+#define WOOD_LABEL_COLOR    (cocos2d::Color4B(0, 228, 255, 255))
 
-#define ENABLED_COLOR       (Color4B::WHITE)
-#define DISABLED_COLOR      (Color4B::RED)
+#define ENABLED_COLOR       (cocos2d::Color4B::WHITE)
+#define DISABLED_COLOR      (cocos2d::Color4B::RED)
 
 #define TOUCH_CANCEL_BY_MOVING_DISTANCE (10.0f)
 
